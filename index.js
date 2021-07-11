b214697fa18186c1e799f6f6dd2bf6e204392f09
John Bouakhasith <johnbeedot@gmail.com>
2021-07-11 11:19:53 -0400

    modified

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const displayValue = document.querySelector('.display-value');
-const button = document.querySelectorAll('.number');
+const btnNumber = document.querySelectorAll('.numberBtn');
+const btnOperator = document.querySelectorAll('.operatorBtn');
+let numberPressed = [];
+let value = [];
+let operator = "";
 
-function add(a, b) {
-    return a + b;
+function add() {
+    let result = value.map(num => num + numberPressed);
+    displayValue.textContent = result.join('');
 };
 
-function subtract(a, b) {
-    return a - b;
+function subtract() {
+
 };
 
 function multiply(a, b) {
@@ -21,9 +26,22 @@ function operate(a, b) {
     return add(a, b);
 };
 
-let value = []
-button.forEach((btn) => btn.addEventListener("click", function(e) {
-    const number = e.target.dataset.number;
-    value.push(number);
-    displayValue.textContent = value;
+// numbers display
+btnNumber.forEach((btn) => btn.addEventListener("click", function(e) {
+    const datasetNumber = e.target.dataset.number;
+    value.push(datasetNumber);
+    numberPressed.push(datasetNumber);
+    displayValue.textContent = value.join('');
+}));
+
+// operators
+btnOperator.forEach((btn) => btn.addEventListener("click", function(e) {
+    const datasetOperator = e.target.dataset.operator;
+    operator = datasetOperator;
+
+    if (operator == "add") {
+        add();
+    } else if (operator == "subtract") {
+        subtract();
+    }
 }));
