28fd46d2cd98b0dadeed69f2241ea27caa007589
John Bouakhasith <johnbeedot@gmail.com>
2021-07-11 20:18:50 -0400

    refactoring

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const numberBtn = document.querySelectorAll('[data-number]')
 const operatorBtn = document.querySelectorAll('[data-operator]')
 const decimalBtn = document.querySelector('[data-decimal]')
 const equalsBtn = document.querySelector('[data-equals]')
+const clearBtn = document.querySelector('[data-clear]')
+const backspaceBtn = document.querySelector('[data-backspace]')
 
 let value = []
 
@@ -23,5 +25,23 @@ function divide(a, b) {
 }
 
 numberBtn.forEach((btn) => btn.addEventListener("click", e => {
-    console.log(e.target.dataset)
+    const number = e.target.dataset.number
+    value += number
+    displayValue.textContent += number
+    console.log(value)
 }))
+
+operatorBtn.forEach((btn) => btn.addEventListener("click", e => {
+    const operator = e.target.dataset.operator
+    console.log(operator)
+}))
+
+decimalBtn.addEventListener("click", e => {
+    const decimal = e.target.dataset.decimal
+    console.log(decimal)
+})
+
+equalsBtn.addEventListener("click", e => {
+    const equals = e.target.dataset.equals
+    console.log(equals)
+})
